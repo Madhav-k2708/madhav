03444b76cac3acf6d8448d97ee555e506e18967b
Madhav-k2708 <madhav.k714@gmail.com>
2025-03-16 13:15:50 +0530

    resume changed

--- components/Headers.jsx
+++ components/Headers.jsx
@@ -46,8 +46,9 @@ const Headers = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1 }}
-          href="/madhav_Resume.pdf"
-          download
+          file
+          href="/MadhavResume.pdf"
+          download="Madhav_WebDeveloper"
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 font-Ovo bg-white dark:text-black"
         >
           my resume <Image src={assets.download_icon} alt="" className="w-4" />
